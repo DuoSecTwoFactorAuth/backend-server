c52d8c14083dd4c34157629d2ea5209c126060f9
Avinash Vijayvargiya <avinash14022002@gmail.com>
2022-10-22 00:26:40 +0530

    initial commit

---
+++ src/main/java/com/duosec/duosecbackend/DuosecBackendApplication.java
@@ -0,0 +1,13 @@
+package com.duosec.duosecbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuosecBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DuosecBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/duosec/duosecbackend/DuosecBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duosec.duosecbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuosecBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
