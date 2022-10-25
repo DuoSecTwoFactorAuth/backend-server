907cc05aedbef61f3011892ffdc80e0c01d10e20
Avinash Vijayvargiya <66490629+avinash14022002@users.noreply.github.com>
2022-10-25 11:03:42 +0530

    CU-2y2zhat_MODEL-Company-Creds-information_Avinash-Vijayvargiya (#8)
    * Added: company creds dao
    
    * Added: company creds dao

---
+++ src/main/java/com/duosec/duosecbackend/dao/CompanyCreds.java
@@ -0,0 +1,24 @@
+package com.duosec.duosecbackend.dao;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+/**
+ * User: Avinash Vijayvargiya
+ * Date: 25-Oct-22
+ * Time: 12:48 AM
+ */
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Document("CompanyCreds")
+public class CompanyCreds {
+    private String companyName;
+    private String companyEmail;
+    private int otpRefreshDuration;
+    private String algorithm;
+    private String password;
+
+}
