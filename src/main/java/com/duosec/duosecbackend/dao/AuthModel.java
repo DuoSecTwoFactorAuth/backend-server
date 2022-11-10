49481d8acaad2724effb74c4ae20587089385129
Avinash Vijayvargiya <avinash14022002@gmail.com>
2022-11-10 12:53:18 +0530

    Convert mongo template to mongo queries

--- src/main/java/com/duosec/duosecbackend/dao/AuthModel.java
+++ src/main/java/com/duosec/duosecbackend/dao/AuthModel.java
@@ -2,15 +2,19 @@ package com.duosec.duosecbackend.dao;
 
 import com.duosec.duosecbackend.model.CompanyCreds;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
 public interface AuthModel extends MongoRepository<CompanyCreds, Integer> {
+    @Query("{companyUniqueId :  ?0}")
     Optional<CompanyCreds> findByCompanyUniqueId(String uniqueId);
 
+    @Query("{companyEmailId :  ?0}")
     Optional<CompanyCreds> findByCompanyEmailId(String companyEmailId);
 
+    @Query("{token :  ?0}")
     CompanyCreds findByToken(String token);
 }
